3429db9704e7f582b7656877bb509ebfd1626289
Olivia Py-Cz <oliviapenycoblentz@gmail.com>
2019-06-29 13:26:51 +0200

    update query param

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -3,27 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Input;
 use App\Ingredient;
 
 class SearchController extends Controller
 {
-    // public function autocomplete() {
-    //     $term = Input::get('term');
-    //     $results = [];
-    //     $queryIngredient = DB::table('ingredients')
-    //         ->where('title', 'LIKE', '%'.$term.'%')->get();
-
-    //     foreach ($queryIngredient as $query) {
-    //         $results[] = ['value' => $query->title ];
-    //     }
-
-    //     $results = array_map("unserialize", array_unique(array_map("serialize", $results)));
-    //     return Response::json($results);
-    // }
 
     public function result(Request $request) {
-        $query = $request->query();
-        $ingredient = Ingredient::where('title', $query)->get();
+        $term = Input::get('search_ing');
+        $ingredient = Ingredient::where('title', 'LIKE', '%'.$term.'%')->get();
         return response()->json($ingredient, 201);
     }
 }
